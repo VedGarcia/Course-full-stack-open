78a29f77fe5512593c201292dd98449e34dd7007
Ved <garciaveduar@gmail.com>
2024-12-23 22:59:03 -0400

    Ejercicio 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,24 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
-      />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -40,12 +30,12 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      {parts.map((part) => (
+        <Part part={part.name} exercises={part.exercises} key={part.name} />
+      ))}
     </div>
   );
 };
@@ -58,6 +48,10 @@ const Part = ({ part, exercises }) => {
   );
 };
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = ({ parts }) => {
+  const sumValues = parts
+    .map((part) => part.exercises)
+    .reduce((a, b) => a + b, 0);
+
+  return <p>Number of exercises {sumValues}</p>;
 };
