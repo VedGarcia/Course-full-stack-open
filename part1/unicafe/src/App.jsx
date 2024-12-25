577aaf1704d789168d8b455feb67a3c1ada91ec0
Ved <garciaveduar@gmail.com>
2024-12-24 22:49:37 -0400

    1.6: unicafe, paso 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,23 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
 
 export default App
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
