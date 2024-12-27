162b43f40cf488d6f18aaf64fb893ee4ace60968
Ved <garciaveduar@gmail.com>
2024-12-26 23:25:58 -0400

    2.1: Información del curso paso 6

---
+++ part2/courseinfo/src/components/Content/index.jsx
@@ -0,0 +1,14 @@
+import Part from '../Part';
+
+const Content = ({ course }) => {
+  const { name, parts } = course;
+    return (
+      <div>
+        {parts.map((part) => (
+          <Part part={part.name} exercises={part.exercises} key={part.name} />
+        ))}
+      </div>
+    );
+  };
+
+  export default Content;
